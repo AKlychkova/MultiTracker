00a0814f8b38b9c1bc3686d8234a0b1fb5958fcf
AKlychkova <adklychkova@edu.hse.ru>
2024-03-15 11:30:25 +0300

    Add packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.hse.multitracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.hse.multitracker"
--- app/src/main/java/ru/hse/multitracker/UI/elements/MainActivity.kt
+++ app/src/main/java/ru/hse/multitracker/UI/elements/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.hse.multitracker
+package ru.hse.multitracker.UI.elements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.hse.multitracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

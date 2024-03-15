8f4e9b280624cafaaf3db0ba0dd4f68033a6ee6c
AKlychkova <adklychkova@edu.hse.ru>
2024-03-15 14:13:12 +0300

    Add navigation API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,18 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.7"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")

3a9b2e1d1933a156537cb8ed29378e6e08722f85
AKlychkova <adklychkova@edu.hse.ru>
2024-03-22 11:10:35 +0300

    Make database singleton

--- app/src/main/java/ru/hse/multitracker/data/database/AppDatabase.kt
+++ app/src/main/java/ru/hse/multitracker/data/database/AppDatabase.kt
@@ -1,14 +1,38 @@
 package ru.hse.multitracker.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import ru.hse.multitracker.data.database.entities.Patient
 import ru.hse.multitracker.data.database.entities.TestSession
 
-@Database(entities = [Patient::class, TestSession::class], version = 1)
+@Database(entities = [Patient::class, TestSession::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun patientDao(): PatientDao
     abstract fun testSessionDao(): TestSessionDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
 }

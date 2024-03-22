8f3c1014900fa1a26a9a422faf7e7032d590fb57
AKlychkova <adklychkova@edu.hse.ru>
2024-03-23 00:14:05 +0300

    Add click listeners in home fragment

--- app/src/main/java/ru/hse/multitracker/ui/elements/HomeFragment.kt
+++ app/src/main/java/ru/hse/multitracker/ui/elements/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import ru.hse.multitracker.R
 import ru.hse.multitracker.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,11 +19,25 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setListeners()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setListeners() {
+        binding.testButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.action_homeFragment_to_settingsFragment
+            )
+        )
+        binding.statisticsButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.action_homeFragment_to_statisticsFragment
+            )
+        )
     }
 }

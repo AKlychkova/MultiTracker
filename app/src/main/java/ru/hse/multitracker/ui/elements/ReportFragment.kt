dcabe4a9fc7a27ac174d09a22b3ef491456078ef
AKlychkova <adklychkova@edu.hse.ru>
2024-03-31 21:30:41 +0300

    Add logic to ReportFragment

--- app/src/main/java/ru/hse/multitracker/ui/elements/ReportFragment.kt
+++ app/src/main/java/ru/hse/multitracker/ui/elements/ReportFragment.kt
@@ -1,16 +1,20 @@
 package ru.hse.multitracker.ui.elements
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import ru.hse.multitracker.R
 import ru.hse.multitracker.databinding.FragmentReportBinding
 import ru.hse.multitracker.ui.view_models.ReportViewModel
+import kotlin.math.roundToInt
 
 class ReportFragment : Fragment() {
-    private lateinit var viewModel: ReportViewModel
+    private val viewModel: ReportViewModel by viewModels { ReportViewModel.Factory }
     private var _binding: FragmentReportBinding? = null
     private val binding get() = _binding!!
 
@@ -20,12 +24,43 @@ class ReportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentReportBinding.inflate(inflater, container, false)
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) { }
+        callback.isEnabled = true
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[ReportViewModel::class.java]
+        setListeners()
+        observeViewModel()
+        val sId = arguments?.getLong("s_id")
+        if (sId == -1L) {
+            val accuracy = arguments?.getFloat("accuracy")
+            val time = arguments?.getInt("time")
+            setText((accuracy!! * 100).roundToInt(), time!!)
+        } else {
+            viewModel.getSession(sId!!)
+        }
+    }
+
+    private fun setText(accuracyPercent: Int, time: Int) {
+        binding.reportAccuracyTextview.text = getString(R.string.percent, accuracyPercent)
+        binding.reportReactionTextview.text = getString(R.string.time, time)
+    }
+
+    private fun setListeners() {
+        binding.homeButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_reportFragment_to_homeFragment)
+        )
+        binding.statisticsButton2.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_reportFragment_to_statisticsFragment)
+        )
+    }
+
+    private fun observeViewModel() {
+        viewModel.currentSession.observe(viewLifecycleOwner) {
+            setText((it.accuracy * 100).roundToInt(), it.reactionTime)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/ru/hse/multitracker/ui/view_models/ReportViewModel.kt
+++ app/src/main/java/ru/hse/multitracker/ui/view_models/ReportViewModel.kt
@@ -1,7 +1,40 @@
 package ru.hse.multitracker.ui.view_models
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.hse.multitracker.MultiTrackerApplication
+import ru.hse.multitracker.data.database.entities.TestSession
+import ru.hse.multitracker.data.repositories.TestSessionRepository
 
-class ReportViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ReportViewModel(private val repository: TestSessionRepository) : ViewModel() {
+    val currentSession = MutableLiveData<TestSession>()
+
+    fun getSession(id:Long) = viewModelScope.launch(Dispatchers.IO) {
+        val session = repository.getSession(id)
+        launch(Dispatchers.Main) {
+            currentSession.value = session
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras
+            ): T {
+                // Get the Application object from extras
+                val application =
+                    checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
+                return ReportViewModel(
+                    (application as MultiTrackerApplication).testSessionRepository
+                ) as T
+            }
+        }
+    }
 }

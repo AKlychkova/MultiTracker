c073f2f741de22c7aa2dc34dc6beb62f9b1fb172
AKlychkova <adklychkova@edu.hse.ru>
2024-05-26 22:36:22 +0300

    Separate animation logic to the TestObjectAnimator class

---
+++ app/src/main/java/ru/hse/multitracker/ui/elements/TestObjectAnimator.kt
@@ -0,0 +1,119 @@
+package ru.hse.multitracker.ui.elements
+
+import android.animation.Animator
+import android.animation.TimeAnimator
+import kotlin.math.atan
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+import kotlin.random.Random
+
+class TestObjectAnimator {
+    fun animate(
+        testSystem: TestSystem,
+        objects: List<Object>,
+        xFrom: Int,
+        xUntil: Int,
+        yFrom: Int,
+        yUntil: Int,
+    ): List<Animator> {
+        val animators: ArrayList<Animator> = ArrayList()
+        // get movement time and speed of current session
+        val time = testSystem.session.movementTime * 1000
+        val speed = testSystem.session.speed
+
+        for (i in objects.indices) {
+            // generate direction
+            objects[i].direction = Random.nextDouble(-Math.PI, Math.PI)
+            // create animator
+            val animator = TimeAnimator()
+            // add listener for every animation frame
+            animator.setTimeListener { _, totalTime, _ ->
+                // check if time has run out
+                if (totalTime >= time) {
+                    animator.end()
+                } else {
+                    // calculate speed projections on the x and y axes
+                    val dx = speed * cos(objects[i].direction).toFloat()
+                    val dy = speed * sin(objects[i].direction).toFloat()
+
+                    // move objects
+                    objects[i].button.x += dx
+                    objects[i].button.y += dy
+
+                    // check for collisions with borders
+                    // left border
+                    if (objects[i].button.x <= xFrom && (objects[i].direction > Math.PI / 2 || objects[i].direction < -Math.PI / 2)) {
+                        objects[i].direction = Math.PI - objects[i].direction
+                    }
+                    // right border
+                    if (objects[i].button.x >= xUntil && (objects[i].direction > -Math.PI / 2 && objects[i].direction < Math.PI / 2)) {
+                        objects[i].direction = Math.PI - objects[i].direction
+                    }
+                    // top border
+                    if (objects[i].button.y <= yFrom && (objects[i].direction < 0 && objects[i].direction > -Math.PI)) {
+                        objects[i].direction = -objects[i].direction
+                    }
+                    // bottom border
+                    if (objects[i].button.y >= yUntil && (objects[i].direction > 0 && objects[i].direction < Math.PI)) {
+                        objects[i].direction = -objects[i].direction
+                    }
+                    // set up the direction so that it is between -pi and pi
+                    while (objects[i].direction > Math.PI) objects[i].direction -= 2 * Math.PI
+                    while (objects[i].direction < -Math.PI) objects[i].direction += 2 * Math.PI
+
+                    // check for collisions with another objects
+                    for (j in i + 1..<objects.size) {
+                        // calculate distance between objects' centres
+                        val xDiff = objects[j].getCentreX() - objects[i].getCentreX()
+                        val yDiff = objects[j].getCentreY() - objects[i].getCentreY()
+                        val distance = sqrt(xDiff * xDiff + yDiff * yDiff)
+                        if (distance <= objects[i].getRadius() + objects[j].getRadius()) {
+
+                            // calculate the angle of the beam (w) from i object center to j object center
+                            var angleW = atan(yDiff.toDouble() / xDiff)
+                            if (objects[j].getCentreX() - objects[i].getCentreX() < 0) {
+                                angleW += Math.PI
+                                while (angleW > Math.PI) angleW -= 2 * Math.PI
+                                while (angleW < -Math.PI) angleW += 2 * Math.PI
+                            }
+                            // angles between w and objects' directions
+                            var w1 = objects[i].direction - angleW
+                            var w2 = objects[j].direction - angleW
+
+                            // speed projections onto the beam w before the collision
+                            var dw1 = speed * cos(w1)
+                            var dw2 = speed * cos(w2)
+
+                            // speed projections onto the perpendicular to the w beam before the collision
+                            val dwt1 = speed * sin(w1)
+                            val dwt2 = speed * sin(w2)
+
+                            // speed projections onto the beam w after the collision are swapped
+                            val temp = dw1
+                            dw1 = dw2
+                            dw2 = temp
+
+                            // calculate angles between w and new directions
+                            w1 = atan(dwt1 / dw1)
+                            if (dw1 < 0) w1 += Math.PI
+                            w2 = atan(dwt2 / dw2)
+                            if (dw2 < 0) w2 += Math.PI
+
+                            // change objects' directions
+                            objects[i].direction = angleW + w1
+                            while (objects[i].direction > Math.PI) objects[i].direction -= 2 * Math.PI
+                            while (objects[i].direction < -Math.PI) objects[i].direction += 2 * Math.PI
+
+                            objects[j].direction = angleW + w2
+                            while (objects[j].direction > Math.PI) objects[j].direction -= 2 * Math.PI
+                            while (objects[j].direction < -Math.PI) objects[j].direction += 2 * Math.PI
+                        }
+                    }
+                }
+            }
+            animators.add(animator)
+        }
+        return animators
+    }
+}
--- app/src/main/java/ru/hse/multitracker/ui/elements/TestingFragment.kt
+++ app/src/main/java/ru/hse/multitracker/ui/elements/TestingFragment.kt
@@ -1,7 +1,6 @@
 package ru.hse.multitracker.ui.elements
 
 import android.animation.Animator
-import android.animation.TimeAnimator
 import android.os.Bundle
 import android.os.SystemClock
 import android.view.LayoutInflater
@@ -18,11 +17,6 @@ import androidx.navigation.fragment.findNavController
 import ru.hse.multitracker.R
 import ru.hse.multitracker.databinding.FragmentTestingBinding
 import ru.hse.multitracker.ui.view_models.TestingViewModel
-import kotlin.math.atan
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
-import kotlin.random.Random
 
 
 class TestingFragment : Fragment() {
@@ -41,7 +35,7 @@ class TestingFragment : Fragment() {
     private var bottomBorder: Int = 0
 
     // animators that are running at the moment
-    private var currentAnimators: ArrayList<Animator> = ArrayList()
+    private var currentAnimators: List<Animator> = emptyList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -99,105 +93,11 @@ class TestingFragment : Fragment() {
             animate()
         }
     }
-    
+
     private fun animate() {
-        // get movement time and speed of current session
-        val time = testSystem.session.movementTime * 1000
-        val speed = testSystem.session.speed
-
-        for (i in objects.indices) {
-            // generate direction
-            objects[i].direction = Random.nextDouble(-Math.PI, Math.PI)
-            // create animator
-            val animator = TimeAnimator()
-            // add listener for every animation frame
-            animator.setTimeListener { _, totalTime, _ ->
-                // check if time has run out
-                if (totalTime >= time) {
-                    animator.end()
-                } else {
-                    // calculate speed projections on the x and y axes
-                    val dx = speed * cos(objects[i].direction).toFloat()
-                    val dy = speed * sin(objects[i].direction).toFloat()
-
-                    // move objects
-                    objects[i].button.x += dx
-                    objects[i].button.y += dy
-
-                    // check for collisions with borders
-                    // left border
-                    if (objects[i].button.x <= 0 && (objects[i].direction > Math.PI / 2 || objects[i].direction < -Math.PI / 2)) {
-                        objects[i].direction = Math.PI - objects[i].direction
-                    }
-                    // right border
-                    if (objects[i].button.x >= rightBorder && (objects[i].direction > -Math.PI / 2 && objects[i].direction < Math.PI / 2)) {
-                        objects[i].direction = Math.PI - objects[i].direction
-                    }
-                    // top border
-                    if (objects[i].button.y <= 0 && (objects[i].direction < 0 && objects[i].direction > -Math.PI)) {
-                        objects[i].direction = -objects[i].direction
-                    }
-                    // bottom border
-                    if (objects[i].button.y >= bottomBorder && (objects[i].direction > 0 && objects[i].direction < Math.PI)) {
-                        objects[i].direction = -objects[i].direction
-                    }
-                    // set up the direction so that it is between -pi and pi
-                    while (objects[i].direction > Math.PI) objects[i].direction -= 2 * Math.PI
-                    while (objects[i].direction < -Math.PI) objects[i].direction += 2 * Math.PI
-
-                    // check for collisions with another objects
-                    for (j in i + 1..<objects.size) {
-                        // calculate distance between objects' centres
-                        val xDiff = objects[j].getCentreX() - objects[i].getCentreX()
-                        val yDiff = objects[j].getCentreY() - objects[i].getCentreY()
-                        val distance = sqrt(xDiff * xDiff + yDiff * yDiff)
-                        if (distance <= objects[i].getRadius() + objects[j].getRadius()) {
-
-                            // calculate the angle of the beam (w) from i object center to j object center
-                            var angleW = atan(yDiff.toDouble() / xDiff)
-                            if (objects[j].getCentreX() - objects[i].getCentreX() < 0) {
-                                angleW += Math.PI
-                                while (angleW > Math.PI) angleW -= 2 * Math.PI
-                                while (angleW < -Math.PI) angleW += 2 * Math.PI
-                            }
-                            // angles between w and objects' directions
-                            var w1 = objects[i].direction - angleW
-                            var w2 = objects[j].direction - angleW
-
-                            // speed projections onto the beam w before the collision
-                            var dw1 = speed * cos(w1)
-                            var dw2 = speed * cos(w2)
-
-                            // speed projections onto the perpendicular to the w beam before the collision
-                            val dwt1 = speed * sin(w1)
-                            val dwt2 = speed * sin(w2)
-
-                            // speed projections onto the beam w after the collision are swapped
-                            val temp = dw1
-                            dw1 = dw2
-                            dw2 = temp
-
-                            // calculate angles between w and new directions
-                            w1 = atan(dwt1 / dw1)
-                            if (dw1 < 0) w1 += Math.PI
-                            w2 = atan(dwt2 / dw2)
-                            if (dw2 < 0) w2 += Math.PI
-
-                            // change objects' directions
-                            objects[i].direction = angleW + w1
-                            while (objects[i].direction > Math.PI) objects[i].direction -= 2 * Math.PI
-                            while (objects[i].direction < -Math.PI) objects[i].direction += 2 * Math.PI
-
-                            objects[j].direction = angleW + w2
-                            while (objects[j].direction > Math.PI) objects[j].direction -= 2 * Math.PI
-                            while (objects[j].direction < -Math.PI) objects[j].direction += 2 * Math.PI
-                        }
-                    }
-                }
-            }
-            currentAnimators.add(animator)
-        }
-
+        val testAnimator = TestObjectAnimator()
+        currentAnimators =
+            testAnimator.animate(testSystem, objects, 0, rightBorder, 0, bottomBorder)
         // set animation end listener
         currentAnimators.getOrNull(0)?.addListener(
             object : Animator.AnimatorListener {
@@ -305,7 +205,7 @@ class TestingFragment : Fragment() {
         val reactionTime =
             ((SystemClock.elapsedRealtime() - binding.chronometer.base) / 1000).toInt()
         // clear list of current animators
-        currentAnimators.clear()
+        currentAnimators = emptyList()
 
         // check if all attempts are finished
         if (testSystem.finishAttempt(reactionTime)) {
